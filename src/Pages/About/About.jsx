f245fd4857a0d6ec8ec958e80609ffe94d6c3e4f
imtiazho <axubair9@gmail.com>
2026-06-18 23:52:16 +0600

    Change data in about

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
 
   const tabs = [
     {
-      name: "Story",
+      name: "Our Story",
       content:
         "We started with a simple promise — to make parcel delivery fast, reliable, and stress-free. Over the years, our commitment to real-time tracking, efficient logistics, and customer-first service has made us a trusted partner for thousands. Whether it's a personal gift or a time-sensitive business delivery, we ensure it reaches its destination — on time, every time.",
     },
